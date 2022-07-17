e3a5b5c10b8906921506e3f668a61a68f198b8ad
TT <46784262+TT-World@users.noreply.github.com>
2022-07-17 10:30:52 +0200

    Done with the Portfolio for now Next time I'm finalizing

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============= typing animation ==============*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer", "YouTuber", "Ethical Hacker","Forex Trader","Guitarist"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
