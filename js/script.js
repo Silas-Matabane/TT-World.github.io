6bf5469efff58a102a2d48cf7820a1221153af36
TT-World <tetelosilas@gmail.com>
2023-06-10 11:52:35 +0200

    New Portfolio layout

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/*============= typing animation ==============*/
-var typed = new Typed(".typing",{
-    strings:["","Web Designer", "YouTuber", "Ethical Hacker","Forex Trader","Guitarist"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true
-})
